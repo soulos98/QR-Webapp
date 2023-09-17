93270695014816d09d9456f2bf74e4cbb96e8563
Jose A. Garcia <101532387+soulos98@users.noreply.github.com>
2023-09-17 16:58:53 -0700

    Added some light logic and more structure to webapp

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static("public/")); // Location of where static files are
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Listening on port number: ${PORT}`);
@@ -9,4 +13,20 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+app.post("/generate", (req, res)=>{
+    let userURL = req.body['UserInput'];
+    let obj = {
+        'qrCode' : userURL
+    }
+    if (!userURL.toLowerCase().includes('.com')){
+        userURL="Error";
+        obj.qrCode = "QR code could not generate, url must contian \'.com\' in string";   
+    }
+
+    console.log(userURL);
+    res.render("index.ejs", obj);
+
+
 })
